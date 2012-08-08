47cfde6ecc5030e2aa443fc7cd59ee94a8f86536
Srini Ranganathan <srinirvasan@gmail.com>
2012-08-08 01:30:51 +0100

    added home to host the main index.html - future wizard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Caliberator::Application.routes.draw do
+
   resources :authors
 
   # match '/books/download' => "books#download"
@@ -59,7 +60,7 @@ Caliberator::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
